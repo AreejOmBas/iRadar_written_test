d2f16bd0b2f1c26ddf53268f3e510cdc5473c27f
Areej Ba Salamah <areej.ombas@gmail.com>
2021-03-10 10:09:27 -0500

    final cleaning

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,54 +1,56 @@
-import React,  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ConditionCard } from "./component/ConditionCard";
-import axios from 'axios';
+import axios from "axios";
 
 import "./App.css";
-import { parse } from "node:url";
 
 
+type D = { [i: string]: any }; // to declare the format of the json data
 
-type D = { [i: string] : any };
+axios.defaults.baseURL = "http://localhost:5000"; // use localhosted server
+//axios.defaults.baseURL = 'aws endpoints here'
 
-axios.defaults.baseURL = 'http://localhost:5000';
+/* 
+  Main App component
+*/
 function App() {
+  const [jsonData, setJsonData] = useState<D>({}); // state for json data requested from the server
+  const [busy, setBusy] = useState<boolean>(true); // flag to wait for the server call to be done before displaying data
+  const [error, setError] = useState<string>(" "); // state to keep store error message
 
-  const [jsonData,setJsonData] = useState<D>({ });
-  const [busy,setBusy] = useState<boolean>(true);
-  const [error, setError] = useState<string>(' ')
-useEffect(()=> {
+// execute the server call request for the data
+  useEffect(() => {
+    axios
+      .get("api/condition")
+      .then((result) => {
+        setJsonData(result.data.conditions);
+        setBusy(false);
+      })
+      .catch((error) => setError(error));
+  }, []);
+// parsing the json data and render it as ConditonCard components
+  const parseData = (data: any) => {
+    const conditions = data
+      ? data.map((condition: any, index: number) => {
+          return (
+            <ConditionCard
+              key={index}
+              label={condition.label}
+              snippet={condition.snippet ? condition.snippet : undefined}
+              image={condition.image ? condition.image : undefined}
+            />
+          );
+        })
+      : { erro: "No Data found" };
 
-axios.get('api/condition')
-.then(result =>  {
-  setJsonData(result.data.conditions);
-    setBusy(false)
-})
-.catch(error => setError(error));
+    return conditions;
+  };
 
-},[]) ;
-
-
-const parseData = (data:any) =>{
-  
-  const conditions =data? data.map((condition :any , index:number) => {
-    return (
-      <ConditionCard
-        label={condition.label}
-        snippet={condition.snippet ? condition.snippet : undefined}
-        image={condition.image ? condition.image : undefined}
-      />
-    );
-  }) : {erro:'No Data found'};
-  
-return conditions;
-}
-
-return (
-  <main className="cards-container">
-
-   { busy ? <p> data is loading </p> : parseData(jsonData)}
-  </main>
-  )
-  
+  return (
+    <main className="cards-container">
+      {busy ? <p> data is loading </p> : parseData(jsonData)}
+    </main>
+  );
 }
 
 export default App;
--- frontEnd/src/component/ConditionCard.tsx
+++ frontEnd/src/component/ConditionCard.tsx
@@ -1,6 +1,10 @@
 import React, { SyntheticEvent } from 'react';
-
 import './ConditionCard.css';
+
+/* 
+  Functional component to represent each condition as a card 
+ */
+
 
 interface Props {
   image?: string;
@@ -11,6 +15,8 @@ interface Props {
 }
 
 export const ConditionCard: React.FC<Props> = ({ image, snippet, label }) => {
+
+  // fall back funtion when an image is not properly loaded , it replaces the src of the image with a generic one
   const addDefaultSrc = (e: SyntheticEvent<HTMLImageElement, Event>) => {
     e.currentTarget.src =
       'https://cdn.pixabay.com/photo/2013/07/18/10/55/dna-163466_1280.jpg';
@@ -18,7 +24,7 @@ export const ConditionCard: React.FC<Props> = ({ image, snippet, label }) => {
 
   return (
     <article className='card'>
-      {image ? (
+      {image ? ( // if image is not available will use a generic one
         <img
           className='card-img'
           onError={addDefaultSrc}
@@ -36,10 +42,10 @@ export const ConditionCard: React.FC<Props> = ({ image, snippet, label }) => {
       <div className='card-body'>
         <p className='card-title'>{label}</p>
 
-        {snippet && (
-          <span> {snippet.substring(0, snippet.lastIndexOf(' ', 50))}.... </span>
+        {snippet && ( // if snippet missing will only show find out more.
+          <span> {snippet.substring(0, snippet.lastIndexOf(' ', 50))}</span>
         )}
-        <a href='#'>find out more.</a>
+        <span>.... </span><a href='#'>find out more.</a>
       </div>
     </article>
   );
