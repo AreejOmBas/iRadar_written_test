881c01ae3c7cea0d9526f7538da3eead6b231176
Areej Ba Salamah <areej.ombas@gmail.com>
2021-03-09 20:04:43 -0500

    final cleaning

--- condition-service/server.ts
+++ condition-service/server.ts
@@ -1,28 +1,31 @@
-import express from 'express';
+/* 
+  Simple server with expressjs and typscript
+*/
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import serverless from 'serverless-http';
+import * as conditions from './data/conditions.json';
 
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000; // used when running without serverless
 
-const db = {
-  conditions: require("../data/conditions")
-}
-
-app.get('/', (req,res ) => {
+// root 
+app.get('/', (req: Request, res: Response ) => {
 
   res.send('Hello from Server');
   
 });
 
-app.get('/api/condition', (req,res ) => {
+// call to get the conditions from json file
+app.get('/api/condition', (req: Request, res: Response ) => {
 
-   res.status(200).json(db.conditions)
+   res.json(conditions)
 
 });
 
-//app.listen(port, () => console.log(`App is lestining on Port ${port}`));
+//app.listen(port, () => console.log(`App is lestining on Port ${port}`)); // used when running without serverless
 
-exports.handler = serverless(app);
+exports.handler = serverless(app); // to be used as the handler with serverless
+
